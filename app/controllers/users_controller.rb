96055adde5a8dab2f2ad069d141401b1825503ad
高橋宗佑 <takahashimunehiro@takahashisousukenoMacBook-Air.local>
2025-09-29 19:49:41 +0900

    ログイン実装でのカラムの追加とバリデーションの追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,12 +11,12 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
+      auto_login(@user) #sorceryのログイン関数
       redirect_to users_path, notice: "ユーザーの登録が完了しました"
     else
       render "new", status: :unprocessable_entity
       #バリデーション失敗時にHTTPステータス(422)を返す
     end
-
   end
 
   def show
@@ -24,9 +24,16 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 
   def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to :users_path, notice: "ユーザーの情報を更新しました"
+    else
+      render "edit", status: :unprocessable_entity
+    end
   end
 
   def destroy
@@ -35,6 +42,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :email)
+    params.require(:user).permit(:name, :email, :avatar, :password, :password_confirmation)
   end
 end
