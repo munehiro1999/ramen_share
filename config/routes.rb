8572ce5f3dbb95a45efbe12d6a20d42fb1d36a3b
高橋宗佑 <takahashimunehiro@takahashisousukenoMacBook-Air.local>
2025-10-01 03:28:02 +0900

    ログイン・ログアウトのルーティング追加

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   post "login", to: "sessions#create"
   delete "logout", to: "sessions#destroy"
 
-  resources :users, only: [:index, :show]
+  resources :users, only: [:index, :show, :edit, :update]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
