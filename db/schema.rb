9c9f0e847df129124e283bf127a06af61b55bc6b
高橋宗佑 <takahashimunehiro@takahashisousukenoMacBook-Air.local>
2025-10-02 20:33:43 +0900

    追加カラムの記載

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_09_29_092352) do
+ActiveRecord::Schema[7.1].define(version: 2025_10_02_062257) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -39,6 +39,17 @@ ActiveRecord::Schema[7.1].define(version: 2025_09_29_092352) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
+  create_table "ramen_posts", force: :cascade do |t|
+    t.string "title"
+    t.integer "genre"
+    t.text "description"
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.float "rating"
+    t.index ["user_id"], name: "index_ramen_posts_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
@@ -51,4 +62,5 @@ ActiveRecord::Schema[7.1].define(version: 2025_09_29_092352) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "ramen_posts", "users"
 end
