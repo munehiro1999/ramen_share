895c6c9eb1eb87aeb581bcb569e608a25d406c79
高橋宗佑 <takahashimunehiro@takahashisousukenoMacBook-Air.local>
2025-10-07 18:19:18 +0900

    ゲストログイン判断メソッド追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,7 @@ class User < ApplicationRecord
   validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
   validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
 
+  def guest?
+    email == "guest@example.com"
+  end
 end
