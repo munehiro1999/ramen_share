4960eacaf7b2b75e251c31955bfffa2315bd8cd3
高橋宗佑 <takahashimunehiro@takahashisousukenoMacBook-Air.local>
2025-10-11 17:34:38 +0900

    Fix appication.js import編集

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,11 +1,11 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
-import "controllers"
+import "./controllers"
 import Rails from "@rails/ujs"
 Rails.start()
 
 import "@popperjs/core"
-import "bootstrap"
+import "./bootstrap"
 import "./map_display"
 import "./map_preview"
 import "./star_rating"
